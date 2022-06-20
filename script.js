0bda327a08b03cf8fbd9ac8f2005ce03fd503165
lunarwriter <sjaierasgb@gmail.com>
2022-06-20 08:01:53 +0000

    Added dataset for matching cards

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let flippedCard = false;
+let firstCard, secondCard;
+
+
 function flipCards() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!flippedCard) {
+        // first click on the cards
+        flippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        flippedCard = false;
+        secondCard = this;
+
+        // do the cards match?
+        console.log(firstCard.dataset.framework);
+        console.log(secondCard.dataset.framework);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCards));
